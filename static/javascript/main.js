6a4914afaf164d80db78bfbcfc57e96c89e9c2df
clapans <clapans555@gmail.com>
2023-04-17 02:08:56 +0900

    230417 카테고리 필터 수정

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -3,13 +3,71 @@ let category = document.querySelector(".category")
 let searchCandidate = document.querySelector("#searchCandidate")
 let searchBarContainer = document.querySelector("#searchBarCtnr")
 let body = document.querySelector(".whole")
+let categoryItem = document.querySelectorAll(".category-item")
+let idx = 1
+let writings = document.querySelectorAll(".write")
+let page = document.querySelectorAll(".pageItem")
 
+document.querySelector(".select-filter").addEventListener("click",() => {
+  let filterLst = []
+  
+  for (let el of document.querySelectorAll(".addFilter"))
+    filterLst.push(el.innerText)
+  
+  sessionStorage.setItem("filter",JSON.stringify(filterLst))
+  setFilter(filterLst,"click")
+})
+
+console.log(sessionStorage.get)
 body.addEventListener("click",() => {
   if (!searchCandidate.classList.contains("d-none")) {
     searchCandidate.classList.add("d-none")
     searchBarContainer.setAttribute("style","border-radius: 30px;")
   }
 })
+
+for (let item of categoryItem)
+  item.addEventListener("click",() => {
+    if (item.classList.contains("addFilter"))
+      item.classList.remove("addFilter")
+    else
+      item.classList.add("addFilter")
+  })
+
+function setFilter(filterLst,str) {
+  writings = []
+  for (let el of document.querySelectorAll(".write")) 
+    if (isIncludeFilter(el,filterLst))
+      writings.push(el)
+
+  if (str === "not")
+    for (let el of document.querySelectorAll(".category-item")) 
+      if (isIncludeLst(el,filterLst))
+        el.classList.add("addFilter")
+
+  if (str === "click") {
+    settings(1)
+    changeSession(1)
+    changeIdx(1)
+    setTotalPost()
+  }
+}
+
+function isIncludeFilter(el,lst) {
+  if (lst.length === 0)
+    return true
+  for (let t of lst) 
+    if (el.classList.contains(t))
+      return true
+  return false
+}
+
+function isIncludeLst(el,lst) {
+  for (let t of lst) 
+    if (el.innerText === t)
+      return true
+  return false
+}
 
 function getCandidate(e) {
   if (!searchCandidate.classList.contains("d-none"))
@@ -32,6 +90,10 @@ function getCandidate(e) {
   }
 }
 
+function setTotalPost() {
+  document.querySelector(".totalWrite").innerText = writings.length + ' 개'
+}
+
 document.querySelector("#searchBar").addEventListener("input",(e) => getCandidate(e))
 document.querySelector("#searchBar").addEventListener("click",() => {
   setTimeout(() => {
@@ -42,7 +104,7 @@ document.querySelector("#searchBar").addEventListener("click",() => {
     },10) 
 })
 
-document.querySelector("#rc").addEventListener("click",() => {
+/*document.querySelector("#rc").addEventListener("click",() => {
   if (recent.classList.contains("d-none")) {
     category.classList.add("d-none")
     recent.classList.remove("d-none")
@@ -63,16 +125,30 @@ document.querySelector("#ct").addEventListener("click",() => {
     document.querySelector("#ct").classList.remove("deactive-page")
     document.querySelector("#rc").classList.add("deactive-page")
   }
-})
-
-let idx = 1
-let writings = document.querySelectorAll(".write")
-let page = document.querySelectorAll(".pageItem")
+})*/
 
 function settings(page) {
+  for (let el of document.querySelectorAll(".write"))
+    if (!el.classList.contains("d-none"))
+      el.classList.add("d-none")
+  
   for (let i = (page-1)*6; i < Math.min(writings.length,page*6); i++) {
-    writings[i].classList.remove("d-none")
+    if (writings[i].classList.contains("d-none"))
+      writings[i].classList.remove("d-none")
   }
+  
+  for (let i = 0; i < writings.length; i++) {
+    writings[i].addEventListener("click", () => { 
+      sessionStorage.setItem("page",parseInt(i/6) + 1)
+    })
+  }
+  
+  let paginates = document.querySelector(".paginates")
+  while (paginates.hasChildNodes()) 
+    paginates.removeChild(paginates.firstChild); 
+
+  if (writings.length === 0)
+    return
   
   for (let i = 0; i < writings.length / 6; i++) {
     let tmp = document.createElement("div")
@@ -83,13 +159,26 @@ function settings(page) {
     num = i + 1
     tmp.innerText = num
     tmp.setAttribute("id","page" + num)
+    tmp.addEventListener("click", () => {
+      changeSession(i+1)
+      changeIdx(i+1)
+    })
     if (i == (page-1)) tmp.classList.add("activePage")
     if (i > 9) tmp.classList.add("d-none")
-    document.querySelector(".paginates").appendChild(tmp)
+    paginates.appendChild(tmp)
+  }
+
+  for (let i = 1; i < writings.length / 6 + 1; i++) {
+    document.querySelector("#page" + i).addEventListener("click", () => {
+      changeSession(i)
+      changeIdx(i)
+    })
   }
 }
 
 function changeIdx(num) {
+  if (writings.length === 0)
+    return
   let tmp = document.querySelectorAll(".pageItem")
   for (let i = 0; i < tmp.length; i++) {
     if (tmp[i].classList.contains("activePage"))
@@ -105,16 +194,21 @@ function changeIdx(num) {
   }
 }
 
+function changeSession(num) {
+  if (sessionStorage.getItem("page") != null) 
+      sessionStorage.setItem("page", num)
+}
+
 if (sessionStorage.getItem("page") != null) 
   idx = parseInt(sessionStorage.getItem("page"))
+else
+  sessionStorage.setItem("page",1)
+
+if (sessionStorage.getItem("filter") != null) {
+  setFilter(JSON.parse(sessionStorage.getItem("filter")),"not")
+}
 
 settings(idx)
-
-for (let i = 1; i < writings.length / 6 + 1; i++) {
-  document.querySelector("#page" + i).addEventListener("click", () => {
-    changeIdx(i)
-  })
-}
 
 document.querySelector("#clickPre").addEventListener("click", () => {
   if (idx != 1) {
@@ -124,6 +218,7 @@ document.querySelector("#clickPre").addEventListener("click", () => {
       for (let i = idx; i < Math.min(page.length + 1, idx + 10); i++) 
         page[i - 1].classList.add("d-none")
     }
+    changeSession(sessionStorage.getItem("page") - 1)
     changeIdx(idx-1)
   }
 })
@@ -136,16 +231,9 @@ document.querySelector("#clickNext").addEventListener("click", () => {
       for (let i = idx - 9; i < idx + 1; i++) 
         page[i - 1].classList.add("d-none")
     }
+    changeSession(sessionStorage.getItem("page") + 1)
     changeIdx(idx+1)
   }
 })
 
-document.querySelector(".totalWrite").innerText = writings.length
-
-let aArr = document.querySelectorAll(".write > a")
-
-for (let i = 0; i < aArr.length; i++) {
-  aArr[i].addEventListener("click", () => {
-    sessionStorage.setItem("page",parseInt(i/6) + 1)
-  })
-}
+setTotalPost()
